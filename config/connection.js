6928a93c0ec46352cb90a3a181dd2694cecbb0d5
kristie fisher <fisherkristie19@icloud.com>
2024-04-01 13:24:46 -0400

    created connection.js, installed needed packages

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,10 @@
 const mongoose = require('mongoose');
 
-// Load environment variables from .env file
-require('dotenv').config();
-
-const { MONGODB_URI, MONGODB_USERNAME, MONGODB_PASSWORD, MONGODB_HOST } = process.env;
-
-// MongoDB connection URI
-const uri = `mongodb://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@${MONGODB_HOST}/${MONGODB_URI}`;
-
-// Connect to MongoDB
-mongoose.connect(uri, {
+// Wrap Mongoose around local connection to MongoDB
+mongoose.connect('mongodb://localhost:27017/userThoughtDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-const db = mongoose.connection;
-
-db.on('error', (error) => console.error('MongoDB connection error:', error));
-db.once('open', () => console.log('Connected to MongoDB'));
-
-module.exports = db;
+// Export connection 
+module.exports = mongoose.connection;
